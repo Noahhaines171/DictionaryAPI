5a363c5ad00f90d0c53ef5f04f1d4a31b4557fc3
Noahhaines171 <Noahhaines171@gmail.com>
2019-06-28 22:28:10 -0400

    Setup for header, search, home-page

--- dictionary-app/src/app/app.module.ts
+++ dictionary-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
