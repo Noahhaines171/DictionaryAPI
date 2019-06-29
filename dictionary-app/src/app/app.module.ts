4bb320bab9d6cd8eb1dab75e6905ecb57ea79ef9
Noahhaines171 <Noahhaines171@gmail.com>
2019-06-28 22:32:54 -0400

    fix importing, show app-home-page

--- dictionary-app/src/app/app.module.ts
+++ dictionary-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './components/home-page/home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 
